c6864f891d151ee36415f8f21e98ed30f4a10ace
Oliwia Pawusiak <trhm@tutamail.com>
2016-05-09 21:14:02 +0200

    Zmiana nazw kolumn z polskiego na angielski w tabeli employees

--- db/schema.rb
+++ db/schema.rb
@@ -25,13 +25,13 @@ ActiveRecord::Schema.define(version: 20160508113046) do
   end
 
   create_table "employees", force: :cascade do |t|
-    t.string   "imie",       limit: 20,  null: false
-    t.string   "nazwisko",   limit: 50,  null: false
-    t.string   "email",                  null: false
-    t.integer  "telefon"
-    t.string   "opis",       limit: 250
+    t.string   "first_name",       limit: 20,  null: false
+    t.string   "last_name",   limit: 50,  null: false
+    t.string   "email",                   default: "", null: false
+    t.integer  "phone_number"
+    t.string   "desc",       limit: 250
     t.string   "password"
-    t.boolean  "czy_admin"
+    t.boolean  "is_admin"
     t.datetime "created_at",             null: false
     t.datetime "updated_at",             null: false
   end
