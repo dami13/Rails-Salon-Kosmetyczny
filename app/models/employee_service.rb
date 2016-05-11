bdfc8c9f44831f9b2c0e136e17571d7e7cf7da98
Olivia <trhm@tutamail.com>
2016-05-11 19:42:03 +0200

    Poprawka relacji w modelach employee, employee_service, service, service_visit i visit

--- app/models/employee_service.rb
+++ app/models/employee_service.rb
@@ -3,6 +3,6 @@ class EmployeeService < ActiveRecord::Base
   self.table_name = "employees_services"
   belongs_to :employee
   belongs_to :service
-  has_many :service_visits
+  belongs_to :service_visit
 
 end
--- app/models/service_visit.rb
+++ app/models/service_visit.rb
@@ -2,6 +2,6 @@ class ServiceVisit < ActiveRecord::Base
 
   self.table_name = "services_visits"
   has_many :employee_services
-  has_many :visits
+  belongs_to :visit
 
 end
