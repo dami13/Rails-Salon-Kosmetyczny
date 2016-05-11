df3ca2a21eb2ca3d7a684764fa5cb9abd799e105
Oliwia Pawusiak <trhm@tutamail.com>
2016-05-11 22:10:05 +0200

    Dodanie validate'ów dla modelu client

--- app/models/client.rb
+++ app/models/client.rb
@@ -2,4 +2,14 @@ class Client < ActiveRecord::Base
 
   has_many :visits
 
+  validates :first_name, :presence => true,
+                         :length => { :maximum => 25 }
+  validates :last_name, :presence => true,
+                        :length => { :maximum => 50 }
+  validates :address, presence: true,
+                      :length => { :maximum => 100 }
+  validates :phone_number, :presence => true,
+                            :length => { :maximum => 12 },
+                            :numericality => true
+
 end
