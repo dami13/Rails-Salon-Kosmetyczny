84c188c20b1d72cb40bdc6dedad475a6bfe8180b
Oliwia Pawusiak <trhm@tutamail.com>
2016-05-11 12:11:04 +0200

    Usunięcie email i password z tabeli employees w schema.db

--- db/schema.rb
+++ db/schema.rb
@@ -27,10 +27,10 @@ ActiveRecord::Schema.define(version: 20160508113046) do
   create_table "employees", force: :cascade do |t|
     t.string   "first_name",       limit: 20,  null: false
     t.string   "last_name",   limit: 50,  null: false
-    t.string   "email",                   default: "", null: false
+    # t.string   "email",                   default: "", null: false
     t.integer  "phone_number"
     t.string   "desc",       limit: 250
-    t.string   "password"
+    # t.string   "password"
     t.boolean  "is_admin"
     t.datetime "created_at",             null: false
     t.datetime "updated_at",             null: false
