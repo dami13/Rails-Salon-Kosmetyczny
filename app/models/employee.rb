af16c719986a1b08b09841367613d0de10221a4e
Oliwia Pawusiak <trhm@tutamail.com>
2016-05-11 21:40:14 +0200

    Dodanie validate'ów do modelu employee

--- app/models/employee.rb
+++ app/models/employee.rb
@@ -5,4 +5,19 @@ class Employee < ActiveRecord::Base
   has_many :service_visits
   has_many :visits, :through => :service_visits
 
+  # has_secure_password
+
+  EMAIL_TEMPLATE = /\A[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}\Z/i
+  validates :first_name, :presence => true,
+                         :length => { :maximum => 25 }
+  validates :last_name, :presence => true,
+                        :length => { :maximum => 50 }
+  # validates :email, :presence => true,
+  #                  :length => { :maximum => 100 },
+  #                  :format => EMAIL_TEMPLATE
+  validates :description, :length => { :maximum => 150 }
+  validates :phone_number, :presence => true,
+                    :length => { :maximum => 12 },
+                    :numericality => true
+
 end
