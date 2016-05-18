12bcfd3d62b759fa9369687dc66b7ba09c04d648
Arkadiusz Pełdiak <apeldiak@wp.pl>
2016-05-18 16:09:17 +0200

    dalsze poprawki logowania

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -2,6 +2,11 @@ class AdminController < ApplicationController
   layout 'admin'
 
   def index
+
+    if !employee_signed_in?
+      redirect_to action: 'login'
+    end
+
   end
   def login
     if employee_signed_in?
