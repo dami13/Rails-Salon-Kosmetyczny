354b94c4bb2322a875ad4067245bad7de8692462
Damian Pasek <>
2016-05-18 15:08:18 +0200

    Przeniesiono def resources do admin_helper.rb

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -9,16 +9,4 @@ module ApplicationHelper
       link_to link_text, link_path
     end
   end
-  
-  def resource_name
-    :employee
-  end
-
-  def resource
-    @resource ||= Employee.new
-  end
-
-  def devise_mapping
-    @devise_mapping ||= Devise.mappings[:employee]
-  end
 end
