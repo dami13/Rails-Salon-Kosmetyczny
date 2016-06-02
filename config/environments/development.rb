2df0288fd99faf03427ce7dc7e724ff3b292ef62
Bartosz Skomro <Skowerko2@gmail.com>
2016-06-02 20:08:08 +0200

    Przypomnienie hasła - mailer

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -42,4 +42,22 @@ Rails.application.configure do
 
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
+  ActionMailer::Base.delivery_method = :smtp
+
+  ActionMailer::Base.smtp_settings = {
+
+  :address => "smtp.gmail.com",
+
+  :port => 587,
+
+  :domain => "gmail.com",
+
+  :authentication => :login,
+
+  :user_name => "salon.kosmetyczny123@gmail.com",
+
+  :password => "ciastko123#"
+
+  }
+
 end
