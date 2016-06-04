b64c42261538db9f66b14892e6d8dfda695e6343
Arkadiusz Pełdiak <apeldiak@wp.pl>
2016-06-04 15:01:03 +0200

    dodanie opcji edytowania wizyt

--- app/controllers/admin/visits_controller.rb
+++ app/controllers/admin/visits_controller.rb
@@ -16,11 +16,11 @@ class Admin::VisitsController < ApplicationController
 
     @events = Array.new
 
-    allvisits.each do | v |
+    allvisits.each do |v|
 
       duration = 0
 
-      v.service_visits.each do | s |
+      v.service_visits.each do |s|
         duration += s.service.duration
       end
 
@@ -51,9 +51,9 @@ class Admin::VisitsController < ApplicationController
       @to = Date.today.at_beginning_of_month.next_month.strftime("%d.%m.%Y")
     end
 
-    @select =  'all'
+    @select = 'all'
     if params[:status]
-      @select =  params[:status]
+      @select = params[:status]
     end
 
     fromf = DateTime.parse(@from)
@@ -64,7 +64,7 @@ class Admin::VisitsController < ApplicationController
       query = query.where(:status => params[:status])
     end
 
-    @select =  params[:status]
+    @select = params[:status]
 
     @visits = query
   end
@@ -74,24 +74,26 @@ class Admin::VisitsController < ApplicationController
   end
 
   def edit
+    @visit = Visit.find(params[:id])
+
+    @se = Service.all.collect { |s| {:id => s.id, :name => s.name, :price => s.price, :duration => s.duration, :employees => s.employees.all.collect { |x| {:id => x.id, :name => x.last_name+' '+x.first_name} }} }
+    @sejson = @se.to_json
+
+    @services = ServiceVisit.select('*').joins(:service, :employee).where(:visit_id => params[:id]).all.collect { |s| {:employee => s.employee_id, :service => s.service_id, :name => s.name, :duration => s.duration, :price => s.price, :employee_name => s.last_name+' '+s.first_name} }
+    @services = @services.to_json
+
+    render 'new'
   end
 
   def update
-  end
 
-  def new
+    @visit = Visit.find(params[:id])
 
-    @visit = Visit.new
-
-    @se = Service.all.collect{|s| {:id => s.id, :name => s.name, :price => s.price, :duration => s.duration, :employees => s.employees.all.collect{|x| {:id => x.id, :name => x.last_name+' '+x.first_name} }}}
+    @se = Service.all.collect { |s| {:id => s.id, :name => s.name, :price => s.price, :duration => s.duration, :employees => s.employees.all.collect { |x| {:id => x.id, :name => x.last_name+' '+x.first_name} }} }
     @sejson = @se.to_json
 
-  end
-
-  def create
-
-    @se = Service.all.collect{|s| {:id => s.id, :name => s.name, :price => s.price, :duration => s.duration, :employees => s.employees.all.collect{|x| {:id => x.id, :name => x.last_name+' '+x.first_name} }}}
-    @sejson = @se.to_json
+    @services = ServiceVisit.select('*').joins(:service, :employee).where(:visit_id => params[:id]).all.collect { |s| {:employee => s.employee_id, :service => s.service_id, :name => s.name, :duration => s.duration, :price => s.price, :employee_name => s.last_name+' '+s.first_name} }
+    @services = @services.to_json
 
     p = params.require(:visit)
 
@@ -102,6 +104,62 @@ class Admin::VisitsController < ApplicationController
       price = 0
     end
 
+    if @visit.update(
+        :client_id => p[:client_id],
+        :price => price,
+        :discount => p[:discount],
+        :comments => p[:comments],
+        :status => p[:status],
+        :start_time => p[:start_time],
+        :sms => p[:sms],
+        :email => p[:email]
+    )
+
+      if p[:services]
+
+        ServiceVisit.where(:visit_id => @visit.id).delete_all
+
+        p[:services].each do |se|
+
+          s = se.split(',')[0]
+          e = se.split(',')[1]
+          @sv = ServiceVisit.new(:client_id => p[:client_id], :employee_id => e, :service_id => s, :visit_id => @visit.id, :client_opinion_rating => 1)
+          @sv.save
+
+        end
+      end
+
+      redirect_to action: 'edit'
+
+    else
+      render 'new'
+    end
+
+
+  end
+
+  def new
+
+    @visit = Visit.new
+
+    @se = Service.all.collect { |s| {:id => s.id, :name => s.name, :price => s.price, :duration => s.duration, :employees => s.employees.all.collect { |x| {:id => x.id, :name => x.last_name+' '+x.first_name} }} }
+    @sejson = @se.to_json
+
+  end
+
+  def create
+
+    @se = Service.all.collect { |s| {:id => s.id, :name => s.name, :price => s.price, :duration => s.duration, :employees => s.employees.all.collect { |x| {:id => x.id, :name => x.last_name+' '+x.first_name} }} }
+    @sejson = @se.to_json
+
+    p = params.require(:visit)
+
+    if p[:services]
+      price = Service.where(:id => p[:services].collect { |s| s.split(',')[0] }).sum(:price)
+      price = (price - (price * (p[:discount].to_i / 100.0))).to_i
+    else
+      price = 0
+    end
 
 
     @visit = Visit.new(
@@ -146,8 +204,8 @@ class Admin::VisitsController < ApplicationController
 
     ids = params[:id].split(',').uniq
 
-    @visits = Visit.select('"visits"."id", "visits"."start_time", SUM("duration") as duration').joins('LEFT JOIN "services_visits" ON "visits"."id"="services_visits"."visit_id"','LEFT JOIN "services" ON "services"."id"="services_visits"."service_id"','LEFT JOIN "employees" ON "employees"."id"="services_visits"."employee_id"').where('services_visits.employee_id' => ids).group('"visits"."id"')
-    @visits = @visits.collect{ |v| { :start => v.start_time.to_time.iso8601, :end => (v.start_time+ v.duration.minutes).to_time.iso8601} }
+    @visits = Visit.select('"visits"."id", "visits"."start_time", SUM("duration") as duration').joins('LEFT JOIN "services_visits" ON "visits"."id"="services_visits"."visit_id"', 'LEFT JOIN "services" ON "services"."id"="services_visits"."service_id"', 'LEFT JOIN "employees" ON "employees"."id"="services_visits"."employee_id"').where('services_visits.employee_id' => ids).group('"visits"."id"')
+    @visits = @visits.collect { |v| {:id => v.id, :start => v.start_time.to_time.iso8601, :end => (v.start_time+ v.duration.minutes).to_time.iso8601} }
 
     render json: @visits
   end
