295e08247776b575bd070671eadd9c46419b1c8a
Damian Pasek <xsadars@interia.pl>
2016-06-06 21:31:18 +0200

    Poprawki w Pracownicy/Usługi

--- app/controllers/admin/employees_services_controller.rb
+++ app/controllers/admin/employees_services_controller.rb
@@ -28,7 +28,7 @@ class Admin::EmployeesServicesController < ApplicationController
     @employee_service = EmployeeService.where("employee_id = ? AND service_id = ?", params[:employee_id], params[:service_id])
     @employee_service.delete_all(service_id: params[:service_id])
 
-    redirect_to action: 'index'
+    redirect_to admin_employees_services_show_remove_path(params[:employee_id])
   end
 
   def create
@@ -36,9 +36,9 @@ class Admin::EmployeesServicesController < ApplicationController
     @employee_service.employee_id = params[:employee_id]
     @employee_service.service_id = params[:service_id]
     if @employee_service.save
-      redirect_to action: 'index'
-    else
       redirect_to admin_employees_services_show_new_path(params[:employee_id])
+    else
+      redirect_to admin_employees_services_index_path()
     end
   end
 end
